b51dda867f5e8ffe50a06bfd566487b5cf56f8a3
Mahi Arora <mahiarora968@gmail.com>
2026-06-23 23:27:47 +0530

    Student Data

--- src/com/abes/FirstJava.java
+++ src/com/abes/FirstJava.java
@@ -1,5 +1,33 @@
 package com.abes;
 
+import com.abes.FirstJava.Student;
+
 public class FirstJava {
+	class Student{
+		int rollNo;
+		String name;
+		 void display() {
+			 System.out.println(rollNo+" "+name);
+		 }
+	};
+// dynamic m method is achieved through main class 
+	//com.abes package collection of class
+public  static class TestStudent{
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stud
+//		System.out.println("Hello Java Program");
+////	variables camel case and classes pascal case
+//		/***Documentation comment*/
+//abstract class means we dont need to create object
+		//interface-> pure abstract class
+		
+		Student s1 =new Student();
+		s1.rollNo=101;
+		s1.name="Rahul";
+		s1.display();
+		return;
+	}
+	
 
 }
+;};
--- src/com/abes/Java.java
+++ src/com/abes/Java.java
@@ -1,5 +1,66 @@
 package com.abes;
 
 public class Java {
+	
 
+import experiment10.BankAccount;
+import experiment10.CurrentAccount;
+import experiment10.SavingsAccount;
+
+// Abstract class
+	abstract class BankAccount {
+		int accountNumber;
+		String accountHolderName;
+		double balance;
+
+		// Constructor
+		BankAccount(int accountNumber, String accountHolderName, double balance) {
+			this.accountNumber = accountNumber;
+			this.accountHolderName = accountHolderName;
+			this.balance = balance;
+		}
+
+		// Deposit Method
+		void deposit(double amount) {
+			balance = balance + amount;
+			System.out.println("Amount Deposited: " + amount);
+		}
+
+		// Display Account Details
+		void displayDetails() {
+			System.out.println("Account Number: " + accountNumber);
+			System.out.println("Account Holder Name: " + accountHolderName);
+			System.out.println("Balance: " + balance);
+		}
+
+		// Abstract Method
+		abstract void calculateInterest();
+	}
+
+	
+
+		void calculateInterest() {
+			double interest = balance * 0.02; // 2% interest
+			System.out.println("Current Account Interest: " + interest);
+		}
+	}
+
+	 // Main class
+	public class Experiment10 {
+		public static void main(String[] args) {
+	 // Creating objects 
+			SavingsAccount sa = new SavingsAccount(101, "Rahul", 10000);
+			CurrentAccount ca = new CurrentAccount(102, "Anita", 20000); 
+			System.out.println("----- Savings Account -----");
+			sa.deposit(2000);
+			sa.displayDetails();
+			sa.calculateInterest();
+			System.out.println();
+			System.out.println("----- Current Account -----");
+			ca.deposit(3000); 
+			ca.displayDetails();
+			ca.calculateInterest();
+			} 
+		}
+		
 }
